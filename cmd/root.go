57608bd97c7866e67b5eed86e5b9858b7646c64c
rakshith <rakshit.s.gowda@gmail.com>
2023-11-30 23:02:45 +0530

    viper configs

---
+++ cmd/root.go
@@ -0,0 +1,25 @@
+package cmd
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/spf13/cobra"
+)
+
+
+var rootCmd = &cobra.Command{
+  Use:   "help",
+  Short: "Help command",
+  Long: `Dispaly help command`,
+  // Run: func(cmd *cobra.Command, args []string) {
+  //   // Do Stuff Here
+  // },
+}
+
+func Execute() {
+  if err := rootCmd.Execute(); err != nil {
+    fmt.Fprintln(os.Stderr, err)
+    os.Exit(1)
+  }
+}
--- main.go
+++ main.go
@@ -1,18 +1,45 @@
 package main
 
 import (
+	"blog/cmd"
+	"blog/config"
 	"fmt"
 
 	"github.com/gin-gonic/gin"
+	"github.com/spf13/viper"
 )
 
-func main() {
+func main(){
+	cmd.Execute()
+}
+
+func serve() {
+	configs := configSet()
 	fmt.Println("Hello world")
 	r := gin.Default()
 	r.GET("/ping", func(c *gin.Context) {
 		c.JSON(200, gin.H{
-			"message": "pong",
+			"message":  "pong",
+			"app name": viper.Get("App.Name"),
 		})
 	})
-	r.Run("localhost:8080")
+	r.Run(fmt.Sprintf("%s:%s", configs.Server.Host, configs.Server.Port))
+}
+
+func configSet() config.Config {
+	viper.SetConfigName("config")
+	viper.SetConfigType("yaml")
+	viper.AddConfigPath("config")
+
+	if err := viper.ReadInConfig(); err != nil {
+		fmt.Println("Error reading the config")
+	}
+
+	var configs config.Config
+
+	err := viper.Unmarshal(&configs)
+	if err != nil {
+		fmt.Printf("unable to decode into struct, %v", err)
+	}
+	return configs
 }
